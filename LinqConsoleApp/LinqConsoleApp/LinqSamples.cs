ec00db4657d26a1b1595abae11cd688cb9c073c0
Piotr Flis <s19841@pjwstk.edu.pl>
2021-01-29 18:16:31 +0100

    Zrobiony podpunkt 1-2

--- LinqConsoleApp/LinqConsoleApp/LinqSamples.cs
+++ LinqConsoleApp/LinqConsoleApp/LinqSamples.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace LinqConsoleApp
 {
-    public class LinqSamples
+    public static class LinqSamples
     {
-        public static IEnumerable<Emp> Emps { get; set; }
-        public static IEnumerable<Dept> Depts { get; set; }
-
-        public LinqSamples()
+        static LinqSamples()
         {
             LoadData();
         }
-
-        public void LoadData()
+        public static IEnumerable<Emp> Emps { get; private set; }
+        public static IEnumerable<Dept> Depts { get; private set; }
+        private static void LoadData()
         {
             var empsCol = new List<Emp>();
             var deptsCol = new List<Dept>();
-
             #region Load depts
             var d1 = new Dept
             {
@@ -26,196 +24,122 @@ namespace LinqConsoleApp
                 Dname = "Research",
                 Loc = "Warsaw"
             };
-
             var d2 = new Dept
             {
                 Deptno = 2,
                 Dname = "Human Resources",
                 Loc = "New York"
             };
-
             var d3 = new Dept
             {
                 Deptno = 3,
                 Dname = "IT",
                 Loc = "Los Angeles"
             };
-
             deptsCol.Add(d1);
             deptsCol.Add(d2);
             deptsCol.Add(d3);
             Depts = deptsCol;
             #endregion
-
             #region Load emps
             var e1 = new Emp
             {
                 Deptno = 1,
                 Empno = 1,
-                Ename = "Jan Kowalski",
+                Ename = "Tomasz Malewski",
                 HireDate = DateTime.Now.AddMonths(-5),
                 Job = "Backend programmer",
                 Mgr = null,
-                Salary = 2000
+                Salary = 4000
             };
-
             var e2 = new Emp
             {
                 Deptno = 1,
                 Empno = 20,
-                Ename = "Anna Malewska",
+                Ename = "Magdalena Kowalska",
                 HireDate = DateTime.Now.AddMonths(-7),
                 Job = "Frontend programmer",
                 Mgr = e1,
-                Salary = 4000
+                Salary = 6000
             };
-
             var e3 = new Emp
             {
                 Deptno = 1,
                 Empno = 2,
-                Ename = "Marcin Korewski",
+                Ename = "Marcin Kwiatkowski",
                 HireDate = DateTime.Now.AddMonths(-3),
                 Job = "Frontend programmer",
                 Mgr = null,
-                Salary = 5000
+                Salary = 8000
             };
-
             var e4 = new Emp
             {
                 Deptno = 2,
                 Empno = 3,
-                Ename = "Paweł Latowski",
+                Ename = "Paweł KOwalski",
                 HireDate = DateTime.Now.AddMonths(-2),
                 Job = "Frontend programmer",
                 Mgr = e2,
-                Salary = 5500
+                Salary = 12000
             };
-
             var e5 = new Emp
             {
                 Deptno = 2,
                 Empno = 4,
-                Ename = "Michał Kowalski",
+                Ename = "Mateusz Maleski",
                 HireDate = DateTime.Now.AddMonths(-2),
                 Job = "Backend programmer",
-                Mgr = e2,
-                Salary = 5500
+                Mgr = null,
+                Salary = 4500
             };
-
             var e6 = new Emp
             {
                 Deptno = 2,
                 Empno = 5,
-                Ename = "Katarzyna Malewska",
+                Ename = "Katarzyna Kos",
                 HireDate = DateTime.Now.AddMonths(-3),
                 Job = "Manager",
                 Mgr = null,
                 Salary = 8000
             };
-
-            var e7 = new Emp
-            {
-                Deptno = null,
-                Empno = 6,
-                Ename = "Andrzej Kwiatkowski",
-                HireDate = DateTime.Now.AddMonths(-3),
-                Job = "System administrator",
-                Mgr = null,
-                Salary = 7500
-            };
-
-            var e8 = new Emp
-            {
-                Deptno = 2,
-                Empno = 7,
-                Ename = "Marcin Polewski",
-                HireDate = DateTime.Now.AddMonths(-3),
-                Job = "Mobile developer",
-                Mgr = null,
-                Salary = 4000
-            };
-
-            var e9 = new Emp
-            {
-                Deptno = 2,
-                Empno = 8,
-                Ename = "Władysław Torzewski",
-                HireDate = DateTime.Now.AddMonths(-9),
-                Job = "CTO",
-                Mgr = null,
-                Salary = 12000
-            };
-
-            var e10 = new Emp
-            {
-                Deptno = 2,
-                Empno = 9,
-                Ename = "Andrzej Dalewski",
-                HireDate = DateTime.Now.AddMonths(-4),
-                Job = "Database administrator",
-                Mgr = null,
-                Salary = 9000
-            };
-
+            
             empsCol.Add(e1);
             empsCol.Add(e2);
             empsCol.Add(e3);
             empsCol.Add(e4);
             empsCol.Add(e5);
             empsCol.Add(e6);
-            empsCol.Add(e7);
-            empsCol.Add(e8);
-            empsCol.Add(e9);
-            empsCol.Add(e10);
             Emps = empsCol;
-
             #endregion
-
         }
-
-
         /*
-            Celem ćwiczenia jest uzupełnienie poniższych metod.
+         *  Celem ćwiczenia jest uzupełnienie poniższych metod.
          *  Każda metoda powinna zawierać kod C#, który z pomocą LINQ'a będzie realizować
          *  zapytania opisane za pomocą SQL'a.
          *  Rezultat zapytania powinien zostać wyświetlony za pomocą kontrolki DataGrid.
          *  W tym celu końcowy wynik należy rzutować do Listy (metoda ToList()).
          *  Jeśli dane zapytanie zwraca pojedynczy wynik możemy je wyświetlić w kontrolce
          *  TextBox WynikTextBox.
-        */
-
+         */
         /// <summary>
-        /// SELECT * FROM Emps WHERE Job = "Backend programmer";
+        ///     SELECT * FROM Emps WHERE Job = "Backend programmer";
         /// </summary>
-        public void Przyklad1()
+        public static void Przyklad1()
         {
-            //var res = new List<Emp>();
-            //foreach(var emp in Emps)
-            //{
-            //    if (emp.Job == "Backend programmer") res.Add(emp);
-            //}
 
-            //1. Query syntax (SQL)
-            var res = from emp in Emps
-                      where emp.Job == "Backend programmer"
-                      select new
-                      {
-                          Nazwisko = emp.Ename,
-                          Zawod = emp.Job
-                      };
-
-
-            //2. Lambda and Extension methods
+            var emps = Emps.Where(emp => emp.Job == "Backend programmer");
+            PrintTaskToConsole(emps);
         }
 
         /// <summary>
-        /// SELECT * FROM Emps Job = "Frontend programmer" AND Salary>1000 ORDER BY Ename DESC;
+        ///     SELECT * FROM Emps Job = "Frontend programmer" AND Salary>1000 ORDER BY Ename DESC;
         /// </summary>
-        public void Przyklad2()
+        public static void Przyklad2()
         {
-            
-
+            var emps = Emps
+                .Where(emp => emp.Job == "Frontend programmer" && emp.Salary > 1000)
+                .OrderByDescending(emp => emp.Ename);
+            PrintTaskToConsole(emps);
         }
 
         /// <summary>
@@ -299,6 +223,12 @@ namespace LinqConsoleApp
         public void Przyklad12()
         {
 
+        }
+        private static void PrintTaskToConsole(IEnumerable<object> iEnumerable)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{new StackTrace().GetFrame(1)?.GetMethod()?.Name}:");
+            iEnumerable.ToList().ForEach(Console.WriteLine);
         }
     }
 }
--- LinqConsoleApp/LinqConsoleApp/Program.cs
+++ LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
-
+using System.Linq;
 namespace LinqConsoleApp
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var d = new LinqSamples();
-            //d.Przyklad1();
+            Console.WriteLine("Emps:");
+            LinqSamples.Emps.ToList().ForEach(Console.WriteLine);
+            Console.WriteLine("\nDepts:");
+            Console.WriteLine();
+            Console.WriteLine("Depts:");
+            LinqSamples.Depts.ToList().ForEach(Console.WriteLine);
+            LinqSamples.Przyklad1();
+            LinqSamples.Przyklad2();
+            LinqSamples.Przyklad3();
+            LinqSamples.Przyklad4();
+            LinqSamples.Przyklad5();
+            LinqSamples.Przyklad6();
+            LinqSamples.Przyklad7();
+            LinqSamples.Przyklad8();
+            LinqSamples.Przyklad9();
+            LinqSamples.Przyklad10();
+            LinqSamples.Przyklad11();
+            LinqSamples.Przyklad12();
         }
     }
 }
